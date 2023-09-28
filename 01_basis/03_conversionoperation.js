baf448636182047c4faeef8075e051cc49a88b2b
Gurdarshan-toor <143855759+Gurdarshan-toor@users.noreply.github.com>
2023-09-28 16:25:41 +0000

    conversion2

--- 01_basis/03_conversionoperation.js
+++ 01_basis/03_conversionoperation.js
@@ -1,4 +1,4 @@
-// let score = "33abc"
+/*// let score = "33abc"
 // let score = false
 
 // console.log( typeof score)
@@ -10,7 +10,7 @@
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true, 0 => false
 // "" => false, 
@@ -20,5 +20,34 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
  
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+*/
+// ====================== operations =========================
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
