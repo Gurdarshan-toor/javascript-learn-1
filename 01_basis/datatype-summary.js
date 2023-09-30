be42c838c06e8a1a334e338792555d4c7a52c104
Gurdarshan-toor <143855759+Gurdarshan-toor@users.noreply.github.com>
2023-09-30 15:06:56 +0000

    datatype-summary

---
+++ 01_basis/datatype-summary.js
@@ -0,0 +1,37 @@
+// primitive 
+
+// 7 type: string, number, boolean, null , undefined, symbol, BigInt
+
+
+
+
+const scroe = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTem = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// const bigNumber = 3214566541233214n
+
+// console.log(id === anotherId);
+
+// Reference (non-primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktimaan", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myFunction);
