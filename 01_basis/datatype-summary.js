b6614afa7752ced4ece74a3b8b6e4869ac236cb8
Gurdarshan-toor <143855759+Gurdarshan-toor@users.noreply.github.com>
2023-09-30 15:44:10 +0000

    slack and heap memery

--- 01_basis/datatype-summary.js
+++ 01_basis/datatype-summary.js
@@ -35,3 +35,27 @@ const myFunction = function(){
 }
 
 console.log(typeof myFunction);
+
+// +++++++++++++++++++++++++++++++++++++++++
+
+// Slack (primitive), Heap (non-primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
