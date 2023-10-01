5fde79a0f0226de802d48200d34a2e8892bd4cbe
Gurdarshan-toor <143855759+Gurdarshan-toor@users.noreply.github.com>
2023-10-01 16:02:41 +0000

    ts

---
+++ 01_basis/05_strings.js
@@ -0,0 +1,29 @@
+const name = "hitesh"
+const ropoCount = 50
+
+// console.log(name + ropoCount + " value");
+
+console.log(`My name is ${name} and my ropo count is ${ropoCount}`);
+
+const gameName = new String('hiteshhc')
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0, 4)
+
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+
+console.log(anotherString);
+
+const newStringOne = "   hitesh "
+console.log(newStringOne);
+console.log(newStringOne.trim());
