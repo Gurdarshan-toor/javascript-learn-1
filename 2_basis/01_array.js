60eeb0212da575c52bfeefb2ca55a29cadfef494
Gurdarshan-toor <143855759+Gurdarshan-toor@users.noreply.github.com>
2023-10-06 17:33:48 +0000

    array

---
+++ 2_basis/01_array.js
@@ -0,0 +1,24 @@
+// array
+
+const myArr = [0, 1, 2, 3, 5]
+
+const myHeros = ["shaktimaan", "naagraj"]
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[0]);
+
+// Array method
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
