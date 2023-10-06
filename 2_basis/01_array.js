39c91d1438a9598af515dcb4bc5db2289642d446
Gurdarshan-toor <143855759+Gurdarshan-toor@users.noreply.github.com>
2023-10-06 17:46:08 +0000

    arryre

--- 2_basis/01_array.js
+++ 2_basis/01_array.js
@@ -4,7 +4,7 @@ const myArr = [0, 1, 2, 3, 5]
 
 const myHeros = ["shaktimaan", "naagraj"]
 const myArr2 = new Array(1, 2, 3, 4)
-console.log(myArr[0]);
+// console.log(myArr[0]);
 
 // Array method
 
@@ -22,3 +22,19 @@ const newArr = myArr.join()
 
 console.log(myArr);
 console.log(newArr);
+
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+
+console.log("B", myArr);
+
+
+const myn2 = myArr.splice(1,3)
+console.log("c", myArr);
+console.log(myn2);
