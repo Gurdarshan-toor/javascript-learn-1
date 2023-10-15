b1b25590a4f306e5555f252f50328124684bc586
Gurdarshan-toor <143855759+Gurdarshan-toor@users.noreply.github.com>
2023-10-15 14:28:27 +0000

    functions.js

---
+++ 03_basis/01_functions.js
@@ -0,0 +1,12 @@
+
+
+function SayMyName() {
+    console.log("H");
+    console.log("i");
+    console.log("t");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+}
+
+SayMyName()
