e484e2eef2386ddb9121d40e50e85f1973431aca
Gurdarshan-toor <143855759+Gurdarshan-toor@users.noreply.github.com>
2023-10-15 15:06:05 +0000

    function

--- 03_basis/01_functions.js
+++ 03_basis/01_functions.js
@@ -1,5 +1,3 @@
-
-
 function SayMyName() {
     console.log("H");
     console.log("i");
@@ -9,4 +7,42 @@ function SayMyName() {
     console.log("h");
 }
 
-SayMyName()
+// SayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    // or
+    return number1 + number2
+}
+
+// addTwoNumbers(3, 4)
+const result = addTwoNumbers(3, 4)
+
+// console.log("Result: ", result);
+
+// function loginUserMessage(username){
+//     return `${username} just logged in`
+// }
+// function loginUserMessage(username){
+//      if(username === undefined){
+//         console.log("Please enter a username");
+//         return
+//      }
+//     return `${username} just logged in`
+// }
+// or
+function loginUserMessage(username = "sam"){
+     if(!username){
+        console.log("Please enter a username");
+        return
+     }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh")); 
+console.log(loginUserMessage("hitesh")); 
