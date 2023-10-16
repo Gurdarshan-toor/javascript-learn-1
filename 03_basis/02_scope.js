dc7e391076f357178e0027f22eee358eb29cea73
Gurdarshan-toor <143855759+Gurdarshan-toor@users.noreply.github.com>
2023-10-16 18:37:47 +0000

    scope level mini hoisting

--- 03_basis/02_scope.js
+++ 03_basis/02_scope.js
@@ -5,10 +5,50 @@ if (true){
     const b = 20
     // var c = 30
     c = 30
-    console.log("Inner", a);
+    // console.log("Inner", a);
 }
  
 // var c = 400
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+// scope level and mini hoisting
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+}
+
+// one()
+
+if (true){
+    const username = "hitesh"
+    if (username == "hitesh"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++++++++++++ interesting ++++++++++++++++++++++
+
+console.log(addone(5))
+function addone (num){
+    return num + 1
+}
+
+
+// addTwo(5) 
+const addTwo = function addone (num){
+    return num + 2
+}
