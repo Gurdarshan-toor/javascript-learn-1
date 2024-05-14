f52873681bc85b634d9e41716edccf2271e4286b
Gurdarshan toor <143855759+Gurdarshan-toor@users.noreply.github.com>
2024-05-14 17:21:18 +0000

    obj

---
+++ 05_iterations_or_loop/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: "swift with apple"
+}
+
+// for (const key in  myObject) {
+//    console.log(key);
+// }
+
+// for (const key in  myObject) {
+//    console.log(myObject[key]);
+// }
+
+// for (const key in  myObject) {
+//    console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     console.log(key);
+// }
+
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
